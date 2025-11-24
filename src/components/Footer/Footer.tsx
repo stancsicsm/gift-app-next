9f016c24a01f0bd2fe10a5e5d02e59cd86651e95
Mate Stancsics <mate.stancsics@tngtech.com>
2025-11-24 21:04:24 +0100

    feat: move footer logo to assets

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import footerLogoSrc from "@/components/Footer/assets/footer-logo.svg";
 import Label from "@/components/Label/Label";
 
 const Footer = () => {
   return (
     <footer className="footer footer-center px-4 pt-12 pb-24">
       <aside>
-        <Image src={footerLogoSrc} alt="Footer Logo" className="w-16 h-auto" />
-        <Label subtle>Developed by Pocok és Társa</Label>
+        <img src="/footer-logo.svg" alt="Footer Logo" className="w-16 h-auto" />
+        <Label subtle>Pocok és Társa</Label>
         <Label subtle className="link pt-4">
           Send us feedback
         </Label>
